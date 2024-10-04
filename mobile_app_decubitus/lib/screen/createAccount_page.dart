a6e65d6592ebaf1fe50f2498d6aa07182e5e20e6
Sorawit Loeyvanicharoen <teekubkub@gmail.com>
2024-10-04 12:46:43 +0700

    update select sex box

--- mobile_app_decubitus/lib/screen/createAccount_page.dart
+++ mobile_app_decubitus/lib/screen/createAccount_page.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'package:mobile_app_decubitus/constant.dart';
 
-class CreateAccountPage extends StatelessWidget {
+class CreateAccountPage extends StatefulWidget {
   CreateAccountPage({super.key});
+
+  @override
+  _CreateAccountPageState createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   // Controllers for text fields
   final TextEditingController _firstNameController = TextEditingController();
@@ -11,63 +18,67 @@ class CreateAccountPage extends StatelessWidget {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
-
-  // Additional controllers for new fields
   final TextEditingController _ssidController = TextEditingController();
   final TextEditingController _sexController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
-
-  // New controller for Date of Birth
   final TextEditingController _dobController = TextEditingController();
+
+  String? selectedGender; // Variable to hold selected gender
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backGroundColor1,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start, // Align to the top
-              children: [
-                const SizedBox(height: 40),
-                _buildTitle(),
-                const SizedBox(height: 8),
-                _buildSubtitle(),
-                const SizedBox(height: 25),
-                _buildTextField(
-                    label: 'SSN ID',
-                    controller: _ssidController), // SSN ID field
-                const SizedBox(height: 16),
-                _buildTextField(
-                    label: 'First Name', controller: _firstNameController),
-                const SizedBox(height: 16),
-                _buildTextField(
-                    label: 'Last Name', controller: _lastNameController),
-                const SizedBox(height: 16),
-                _buildTextField(
-                    label: 'Sex', controller: _sexController), // Sex field
-                const SizedBox(height: 16),
-                _buildTextField(
-                    label: 'Phone',
-                    controller: _phoneController), // Phone field
-                const SizedBox(height: 16),
-                _buildDateOfBirthField(context),
-                const SizedBox(height: 16),
-                _buildTextField(
-                    label: 'Password',
-                    controller: _passwordController,
-                    obscureText: true),
-                const SizedBox(height: 16),
-                _buildTextField(
-                    label: 'Confirm Password',
-                    controller: _confirmPasswordController,
-                    obscureText: true),
-                const SizedBox(height: 20),
-                _buildCreateAccountButton(context),
-                const SizedBox(height: 100),
-              ],
+      resizeToAvoidBottomInset: true,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey, // Use Form widget
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 40),
+                    _buildTitle(),
+                    const SizedBox(height: 8),
+                    _buildSubtitle(),
+                    const SizedBox(height: 25),
+                    _buildTextField(
+                        label: 'SSN ID', controller: _ssidController),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                        label: 'First Name', controller: _firstNameController),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                        label: 'Last Name', controller: _lastNameController),
+                    const SizedBox(height: 16),
+                    _buildGenderSelection(), // Add gender selection here
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                        label: 'Phone', controller: _phoneController),
+                    const SizedBox(height: 16),
+                    _buildDateOfBirthField(context),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                        label: 'Password',
+                        controller: _passwordController,
+                        obscureText: true),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                        label: 'Confirm Password',
+                        controller: _confirmPasswordController,
+                        obscureText: true),
+                    const SizedBox(height: 20),
+                    _buildCreateAccountButton(context),
+                    const SizedBox(height: 30),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
@@ -129,6 +140,74 @@ class CreateAccountPage extends StatelessWidget {
     );
   }
 
+  // Method to build Gender Selection
+  Widget _buildGenderSelection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Male"; // Set selected gender
+                _sexController.text = selectedGender!; // Update controller
+              });
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                  vertical: 15.0), // Add padding for better touch area
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: selectedGender == "Male"
+                      ? tertiaryColor
+                      : secondaryColor, // Change to red if selected
+                  border: Border.all(
+                      color: selectedGender == "Male"
+                          ? primaryColor
+                          : secondaryColor)),
+              child: Center(
+                  child: Text('Male',
+                      style: TextStyle(
+                          color: selectedGender == "Male"
+                              ? Colors.black
+                              : Colors.black))),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10), // Space between buttons
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Female"; // Set selected gender
+                _sexController.text = selectedGender!; // Update controller
+              });
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                  vertical: 15.0), // Add padding for better touch area
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: selectedGender == "Female"
+                      ? tertiaryColor
+                      : secondaryColor, // Change to red if selected
+                  border: Border.all(
+                      color: selectedGender == "Female"
+                          ? primaryColor
+                          : secondaryColor)),
+              child: Center(
+                  child: Text('Female',
+                      style: TextStyle(
+                          color: selectedGender == "Female"
+                              ? Colors.black
+                              : Colors.black))),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   // Method to build Date of Birth field with a date picker
   Widget _buildDateOfBirthField(BuildContext context) {
     return GestureDetector(
@@ -147,9 +226,11 @@ class CreateAccountPage extends StatelessWidget {
             ),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8.0),
-              borderSide: const BorderSide(color: primaryColor),
+              borderSide: const BorderSide(
+                  color: primaryColor), // Change color here if needed
             ),
-            contentPadding: const EdgeInsets.all(15),
+            contentPadding:
+                const EdgeInsets.all(15), // Adjust padding as necessary
           ),
         ),
       ),
@@ -165,28 +246,20 @@ class CreateAccountPage extends StatelessWidget {
       lastDate: DateTime.now(),
     );
     if (picked != null) {
-      // Format and set the selected date in YYYY-MM-DD format
       String formattedDate = "${picked.toLocal()}".split(' ')[0];
-      _dobController.text = formattedDate; // Ensure this is set
+      _dobController.text = formattedDate;
     }
   }
 
-  // Method to validate input fields
-// Method to validate input fields
+  // Method to validate input fields (if needed)
   bool _validateInputs(BuildContext context) {
     final String firstName = _firstNameController.text.trim();
     final String lastName = _lastNameController.text.trim();
     final String password = _passwordController.text.trim();
     final String confirmPassword = _confirmPasswordController.text.trim();
 
-    // Validate SSID
     final String ssid = _ssidController.text.trim();
-
-    // Validate Phone
     final String phone = _phoneController.text.trim();
-
-    // Validate Date of Birth
-    final String dob = _dobController.text.trim(); // Get DOB from controller
 
     if (ssid.isEmpty) {
       _showSnackBar(context, 'SSID cannot be empty');
@@ -203,19 +276,19 @@ class CreateAccountPage extends StatelessWidget {
       return false;
     }
 
-    if (phone.isEmpty || phone.length < 10) {
-      // Assuming phone should be at least 10 digits
-      _showSnackBar(context, 'Phone number must be at least 10 digits');
+    if (phone.isEmpty || phone.length < 5) {
+      // Assuming phone should be at least10 digits
+      _showSnackBar(context, 'Phone number must be at least10 digits');
       return false;
     }
 
     if (_dobController.text.isEmpty) {
       _showSnackBar(context, 'Date of Birth cannot be empty');
-      return false; // Prevent further execution if DOB is empty
+      return false;
     }
 
     if (password.isEmpty || password.length < 6) {
-      _showSnackBar(context, 'Password must be at least 6 characters');
+      _showSnackBar(context, 'Password must be at least6 characters');
       return false;
     }
 
@@ -224,17 +297,16 @@ class CreateAccountPage extends StatelessWidget {
       return false;
     }
 
-    return true; // All validations passed
+    return true;
   }
 
-  // Method to show SnackBar for feedback
+// Method to show SnackBar for feedback
   void _showSnackBar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message)),
-    );
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
-  // Modified method to build the Create Account button
+// Modified method to build the Create Account button
   Widget _buildCreateAccountButton(BuildContext context) {
     return ElevatedButton(
       onPressed: () async {
@@ -248,14 +320,12 @@ class CreateAccountPage extends StatelessWidget {
               ssnId: int.parse(_ssidController.text.trim()),
               sex: _sexController.text.trim(),
               phone: int.parse(_phoneController.text.trim()),
-              dateOfBirth:
-                  _dobController.text.trim(), // Ensure this is included
+              dateOfBirth: _dobController.text.trim(),
               profileImage: '',
               roleId: 1,
             );
             _showSnackBar(context, 'Account Created!');
-            Navigator.pushNamed(
-                context, '/login'); // Navigate after successful registration
+            Navigator.pushNamed(context, '/login');
           } catch (e) {
             _showSnackBar(context, e.toString());
           }
