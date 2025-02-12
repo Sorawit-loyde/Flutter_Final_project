0a460e0fae67c8a3a330108f9aa3943a025318eb
Sorawit Loeyvanicharoen <teekubkub@gmail.com>
2025-02-12 15:01:39 +0700

    update

--- mobile_app_decubitus/lib/screen/patient/profile_Content.dart
+++ mobile_app_decubitus/lib/screen/patient/profile_Content.dart
@@ -285,7 +285,7 @@ class _ProfileContentState extends State<ProfileContent> {
                                     ),
                             ),
                           );
-                        }).toList(),
+                        }),
                         if (!isEditing)
                           Center(
                             child: ElevatedButton(
